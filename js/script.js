2da2a7bd3ef5a0a3a311a8de6b370bc7edb281ce
BiagioCaputo <scebiagio98@gmail.com>
2023-12-20 15:18:31 +0100

    terminata versione javascript con stampa in console

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
+console.log("JS OK")
+
+//variabili
 
 
+
+
+//ciclo for per i numeri da 1 a 100
+
+for(let i = 1; i <= 100; i++){ 
+
+     //inserisco la i in una variabile per non interrompere il ciclo
+
+    let number = i;  
+    
+    //impongo le mie condizioni 
+
+    if(i % 3 === 0){   
+        number = "fizz"; 
+    }
+
+    if(i % 5 === 0){
+        number = "buzz";
+    }
+
+    if(i % 5 === 0 && i % 3 === 0){
+        number = "fizzbuzz";
+    }
+
+    //stampo nella console la mia lista
+    console.log(number)
+    
+} 
