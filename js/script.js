b8e8a1916e88df578be5e3a05659bd2709a5f483
BiagioCaputo <scebiagio98@gmail.com>
2023-12-20 15:54:31 +0100

    bonus 1 completato

--- js/script.js
+++ js/script.js
@@ -4,12 +4,18 @@ console.log("JS OK")
 
 
 
+//recupero il contenitore della mia lista
+
+const containerUl = document.getElementById("ul-container")
+
+//inizializzo la mia lista
+
+let listNumbers = "<ul>";
 
 //ciclo for per i numeri da 1 a 100
-
 for(let i = 1; i <= 100; i++){ 
 
-     //inserisco la i in una variabile per non interrompere il ciclo
+    //inserisco la i in una variabile per non interrompere il ciclo
 
     let number = i;  
     
@@ -26,8 +32,13 @@ for(let i = 1; i <= 100; i++){
     if(i % 5 === 0 && i % 3 === 0){
         number = "fizzbuzz";
     }
-
-    //stampo nella console la mia lista
-    console.log(number)
-    
+    //salvo la mia variabile nel mio li
+    listNumbers += ` <li>${number}</li>` ;
 } 
+
+//chiudo la mia lista
+listNumbers += "</ul>";
+
+//pubblico la mia lista nell'html
+containerUl.innerHTML = listNumbers;
+    
