fbaa33ac7fc2c116fc7037f5362502a4d9ed8fc4
BiagioCaputo <scebiagio98@gmail.com>
2023-12-20 16:47:20 +0100

    Terminato bonus 2 modificando il js e creando le classi background nel css

--- js/script.js
+++ js/script.js
@@ -19,22 +19,28 @@ for(let i = 1; i <= 100; i++){
 
     let number = i;  
     
-    //impongo le mie condizioni 
+    //impongo le mie condizioni e a seconda dello stato della variabile stampo in maniera differente i miei li con i rispettivi background
 
     if(i % 3 === 0){   
         number = "fizz"; 
+        listNumbers += ` <li class="square bg-square-fizz d-flex align-items-center justify-content-center"><span>${number}</span></li>` ;
     }
 
-    if(i % 5 === 0){
+    else if(i % 5 === 0){
         number = "buzz";
+        listNumbers += ` <li class="square bg-square-buzz d-flex align-items-center justify-content-center"><span>${number}</span></li>` ;
     }
 
-    if(i % 5 === 0 && i % 3 === 0){
+    else if(i % 5 === 0 && i % 3 === 0){
         number = "fizzbuzz";
+        listNumbers += ` <li class="square bg-square-fizz-buzz d-flex align-items-center justify-content-center"><span>${number}</span></li>` ;
     }
-    //salvo la mia variabile nel mio li
-    listNumbers += ` <li class="square d-flex align-items-center justify-content-center"><span>${number}</span></li>` ;
-} 
+
+    else{
+        listNumbers += ` <li class="square bg-square-blu d-flex align-items-center justify-content-center"><span>${number}</span></li>` ;
+    }
+
+}
 
 //chiudo la mia lista
 listNumbers += "</ul>";
