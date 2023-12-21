169dd1d40ba14ba26dd94e99a07ac506e9e2949b
BiagioCaputo <scebiagio98@gmail.com>
2023-12-21 15:17:24 +0100

    Correzione ordine if/else

--- js/script.js
+++ js/script.js
@@ -20,8 +20,13 @@ for(let i = 1; i <= 100; i++){
     let number = i;  
     
     //impongo le mie condizioni e a seconda dello stato della variabile stampo in maniera differente i miei li con i rispettivi background
+    
+    if(i % 5 === 0 && i % 3 === 0){
+        number = "fizzbuzz";
+        listNumbers += ` <li class="square bg-square-fizz-buzz d-flex align-items-center justify-content-center"><span>${number}</span></li>` ;
+    }
 
-    if(i % 3 === 0){   
+    else if(i % 3 === 0){   
         number = "fizz"; 
         listNumbers += ` <li class="square bg-square-fizz d-flex align-items-center justify-content-center"><span>${number}</span></li>` ;
     }
@@ -29,11 +34,6 @@ for(let i = 1; i <= 100; i++){
     else if(i % 5 === 0){
         number = "buzz";
         listNumbers += ` <li class="square bg-square-buzz d-flex align-items-center justify-content-center"><span>${number}</span></li>` ;
-    }
-
-    else if(i % 5 === 0 && i % 3 === 0){
-        number = "fizzbuzz";
-        listNumbers += ` <li class="square bg-square-fizz-buzz d-flex align-items-center justify-content-center"><span>${number}</span></li>` ;
     }
 
     else{
